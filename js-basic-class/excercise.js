c9c13918d1f593047f813960a2cc2d4b1e898d6c
Cotbert2 <mateojgdev@gmail.com>
2025-01-06 10:32:54 -0500

    exercise

--- js-basic-class/excercise.js
+++ js-basic-class/excercise.js
@@ -6,24 +6,21 @@ const isNumber = (num) => typeof num === 'number';
 
 function calcularPromedio (num1,num2,num3) {
     if (!isNumber(num1) || !isNumber(num2) || !isNumber(num3)) {
-        console.log('Error: one or more arguments are not numbers');
-        return;
+        throw new Error('Error: one or more arguments are not numbers');
     }
     return (num1+num2+num3)/3;
 }
 
 const determinarMayor = function (num1,num2) {
     if (!isNumber(num1) || !isNumber(num2)) {
-        console.log('Error: one or more arguments are not numbers');
-        return;
+        throw new Error('Error: one or more arguments are not numbers');
     }
     return num1>num2 ? num1 : num2;
 }
 
 const esPar = (num) => {
     if (!isNumber(num)) {
-        console.log('Error: argument is not a number');
-        return;
+        throw new Error('Error: one or more arguments are not numbers');
     }
     return num%2 === 0;
 };
@@ -32,5 +29,5 @@ const esPar = (num) => {
     console.log('Promedio:', calcularPromedio(num1,num2,num3));
     console.log('Mayor:', determinarMayor(num1,num2));
     console.log('Es par:', esPar(num1));
-})(1,2,3);
+})(1,'',3);
 
